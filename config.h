3748715edaa9f8040c72fbc881ebff5239743b17
Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>
2023-01-06 14:12:55 +0100

    use a static version for Config and config.h
    This avoids calling Configure script which can fail now we have removed
    the other tools of the netsniff-ng projet. This also allows us to define
    custom DEFINE in config.h.
    
    Enable ENABLE_TX_TIMESTAMPS in config.h.
    
    SFL: #8529
    Change-Id: I16694abf8d9815edb69926b0eeac7fc474c26af8
    Signed-off-by: Mathieu Dupré <mathieu.dupre@savoirfairelinux.com>

---
+++ config.h
@@ -0,0 +1,17 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+#define FILE_CLIENTS ".curvetun/clients"
+#define FILE_SERVERS ".curvetun/servers"
+#define FILE_PRIVKEY ".curvetun/priv.key"
+#define FILE_PUBKEY ".curvetun/pub.key"
+#define FILE_USERNAM ".curvetun/username"
+#define GITVERSION "v0.6.7-26-gb23c151"
+#define HAVE_LIBNL 1
+#define HAVE_TCPDUMP_LIKE_FILTER 1
+#define HAVE_GEOIP 1
+#define HAVE_LIBZ 1
+#define HAVE_HARDWARE_TIMESTAMPING 1
+#define HAVE_TPACKET3 1
+
+#define ENABLE_TX_TIMESTAMPS 1
+#endif /* CONFIG_H */
